de4a1b84681aec8fd143ff6d29798c9d27624a23
AlexDrotenko <92333185+AlexDrotenko@users.noreply.github.com>
2021-10-11 23:04:15 +0300

    first commit

--- app/calc.js
+++
@@ -1,9 +0,0 @@
-const neatCsv = require('neat-csv');
-const fs = require('fs')
-fs.readFile(’./acme_worksheet.csv’, async (err, data) => {
-if (err) {
-console.error(err)
-return
-}
-console.log(await neatCsv(data))
-})
